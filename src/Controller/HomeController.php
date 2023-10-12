00acecd7651cff273df88c63a709db0aa4b43943
ars1992 <alessandro.salomon@gmx.de>
2023-10-12 18:49:15 +0200

    alle abos lesen und ein abo hinzufügen möglich

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,34 +5,53 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Entity\Sub;
+use DateTime;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class HomeController extends AbstractController 
 {
     #[Route('/home', name: 'app_home')]
-    public function index(): JsonResponse
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $sub = $entityManager->getRepository(Sub::class)->findAll();
+
+        if( ! $sub)
+        {
+            return $this->json(["succes" => false], 418);
+        }
+
         $dataArray = [
             "success" => false,
-            "sub" => $this->generateSubs(),
+            "sub" => $sub,
         ];
 
         return $this->json($dataArray);
     }
 
-    protected function generateSubs(): array
+    public function addSub(Request $request, EntityManagerInterface $entityManager): Response
     {
-        $returnArray = [];
+        $subName = $request->request->get("name");
+        $subStartdate = $request->request->get("startdate");
 
         $sub = (new Sub)
-        ->setName("Netflix")
-        ->setStartDate(new \DateTime);
+        ->setName($subName)
+        ->setStartDate(new DateTime($subStartdate));
+        
+        $entityManager->persist($sub);
+        $entityManager->flush();
 
-        $returnArray[] = $sub;
+        if($sub->getId())
+        {
+            return $this->json(["succes" => true, "sub" => $sub], 201);
+        }
 
-
-        return $returnArray;
+        return $this->json(["succes" => false], 418);
     }
 }
