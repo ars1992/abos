8f8a62f999c1e1d062ebda2af0641dfef881ffd3
ars1992 <alessandro.salomon@gmx.de>
2023-10-12 19:40:49 +0200

    twigbundle instaliert

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,18 +12,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
-class HomeController extends AbstractController 
+class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
     public function index(EntityManagerInterface $entityManager): Response
     {
         $sub = $entityManager->getRepository(Sub::class)->findAll();
 
-        if( ! $sub)
-        {
+        if (!$sub) {
             return $this->json(["succes" => false], 418);
         }
 
@@ -35,23 +34,27 @@ class HomeController extends AbstractController
         return $this->json($dataArray);
     }
 
-    public function addSub(Request $request, EntityManagerInterface $entityManager): Response
+    public function addSub(Request $request, EntityManagerInterface $entityManager, ValidatorInterface $validator): Response
     {
         $subName = $request->request->get("name");
         $subStartdate = $request->request->get("startdate");
 
         $sub = (new Sub)
-        ->setName($subName)
-        ->setStartDate(new DateTime($subStartdate));
-        
-        $entityManager->persist($sub);
-        $entityManager->flush();
+            ->setName($subName)
+            ->setStartDate(new DateTime($subStartdate));
 
-        if($sub->getId())
-        {
+        $errors = $validator->validate($sub);
+
+        if (count($errors) <= 0) {
+            $entityManager->persist($sub);
+            $entityManager->flush();
             return $this->json(["succes" => true, "sub" => $sub], 201);
         }
 
-        return $this->json(["succes" => false], 418);
+        return $this->render('author/validation.html.twig', [
+            'errors' => $errors,
+        ]);
+
+        #return $this->json(["succes" => false], 418);
     }
 }
--- src/Entity/Sub.php
+++ src/Entity/Sub.php
@@ -6,6 +6,8 @@ use App\Repository\SubRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 #[ORM\Entity(repositoryClass: SubRepository::class)]
 class Sub implements \JsonSerializable
 {
@@ -15,6 +17,7 @@ class Sub implements \JsonSerializable
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
     private ?string $name = null;
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
