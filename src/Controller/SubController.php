1bcf64c7b85c61a7361355107513ca735d09cd6b
ars1992 <alessandro.salomon@gmx.de>
2023-10-16 16:11:35 +0200

    bug fix

--- src/Controller/SubController.php
+++ src/Controller/SubController.php
@@ -15,7 +15,7 @@ use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class SubController extends AbstractController
 {
-    #[Route('/sub', name: 'app_sub')]
+    //#[Route('/sub', name: 'app_sub')]
     public function list(EntityManagerInterface $entityManager): Response
     {
         $subs = $entityManager->getRepository(Sub::class)->findAll();
@@ -33,7 +33,6 @@ class SubController extends AbstractController
 
     public function create(Request $request, EntityManagerInterface $entityManager, ValidatorInterface $validator): Response
     {
-
         $subName = $request->request->get("name");
         $subStartdate = $request->request->get("startDate");
 
@@ -46,13 +45,12 @@ class SubController extends AbstractController
         if (count($errors) <= 0) {
             $entityManager->persist($sub);
             $entityManager->flush();
-            return $this->json(["succes" => true, "data" => $sub], 201);
+            return $this->json(["succes" => true, "sub" => $sub], 201);
         }
 
         return $this->render('author/validation.html.twig', [
             'errors' => $errors,
         ]);
-
     }
 
     public function update(int $id, Request $request, EntityManagerInterface $entityManager, ValidatorInterface $validator): Response
